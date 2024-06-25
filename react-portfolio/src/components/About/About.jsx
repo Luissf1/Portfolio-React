58e48d39f9989b0e9b27101742bd7d3be1481050
Luis Adrian Silva Reyes <54382882+Luissf1@users.noreply.github.com>
2024-06-25 11:13:27 -0700

    skill update

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -17,21 +17,36 @@ export const About = () => {
             <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" />
             <div className={styles.aboutItemText}>
               <h3>Frontend Developer</h3>
-              <p>I'm a frontend Developer</p>
+              <p>
+                I am a passionate frontend developer with a knack for creating
+                visually appealing and highly functional user interfaces. With a
+                strong foundation in HTML, CSS, and JavaScript, I specialize in
+                crafting responsive and interactive web applications. 
+              </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/serverIcon.png")} alt="Server Icon" />
             <div className={styles.aboutItemText}>
               <h3>Backend Developer</h3>
-              <p>I'm a Backend Developer</p>
+              <p>
+                I am a dedicated backend developer specializing in C# and SQL.
+                With a solid foundation in building efficient and scalable
+                server-side applications, I excel at creating robust solutions
+                that drive seamless performance. 
+              </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/uiIcon.png")} alt="Ui Icon" />
             <div className={styles.aboutItemText}>
               <h3>UI Designer</h3>
-              <p>I have designed various landing pages</p>
+              <p>
+                I am a passionate UI designer with a talent for creating
+                intuitive and visually appealing user interfaces. Specializing
+                in mobile and web prototypes, I leverage Figma to design
+                interactive and user-friendly experiences.
+              </p>
             </div>
           </li>
         </ul>
--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -7,10 +7,25 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Luis</h1>
-        <p className={styles.description}>I'm a data analyst</p>
-        <a href="mailto:luis.silva1252@gmail.com" className={styles.contactBtn}>Contact Me</a>
+        <p className={styles.description}>
+          I am a versatile developer specializing in both web and mobile
+          applications. With expertise in C#, Flutter, and PHP Laravel, I create
+          dynamic and user-friendly solutions across various platforms. My
+          experience in SQL database management ensures robust and efficient
+          data handling for all my projects. Whether building from the ground up
+          or enhancing existing applications, I am committed to delivering
+          high-quality, scalable, and maintainable software that meets diverse
+          client needs.
+        </p>
+        <a href="mailto:luis.silva1252@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
       </div>
-      <img src={getImageUrl("hero/heroImage.png")} alt="Hero image" className={styles.heroImg}/>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image"
+        className={styles.heroImg}
+      />
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
     </section>
